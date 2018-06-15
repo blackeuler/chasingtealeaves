e80c8ecd768c261d4a856d5d6a19c3e24487448d
Christopher David Miller <millerc8@allegheny.edu>
2018-06-15 14:45:35 -0400

    added the link to wireframe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App bg">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">CHASING THE TEA LEAVES:</h1>
+          <h1 className="App-title">MY JOURNEY EAST</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
