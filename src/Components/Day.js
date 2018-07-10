c4170db5b3037bc44a2b8bba8549d454e8bd3309
Christopher David Miller <millerc8@allegheny.edu>
2018-07-10 09:05:56 -0400

    Fixing layout&trying to improve load time

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -8,7 +8,7 @@ export const Day = (props) =>{
       }
     return(
         
-         <div id={props.iD}onClick={props.onClickDay}role="img" aria-label={`${props.name}`} className=" link  dt  hide-child br2 cover bg-center grow br100 w-100 h100 ma1 card  ba bw b--pin" style={styles} >
+         <div id={props.iD}onClick={props.onClickDay}role="img" aria-label={`${props.name}`} className=" link  dt  hide-child br2 cover bg-center grow br100   ma1 card  ba bw b--pin" style={styles} >
             <span className="yellow f3 white dtc v-mid  child bg-black-40 pa5 bg-animate hover-bg-dark-red  v-mid  ">
                 <p>
                     {`Day ${props.iD}`}
